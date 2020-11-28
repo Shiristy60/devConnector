fa4d15a4c90a17842dc35e212a36c48f638f6701
Shiristy60 <shiristykotnala1.1@gmail.com>
2020-11-28 14:40:44 +0530

    User Model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 // @route       GET api/users/test
 // @desc        tests users route
@@ -7,5 +10,39 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
     msg: 'Users Works'
 }));
+
+// @route       GET api/users/register
+// @desc        register a user
+// @access      Public Route
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(user) {
+                return res.status(404).json({email: 'email already exists'});
+            } else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', // size
+                    r: 'pg', // rating
+                    d: 'mm'  // default
+                })
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                });
+                //hashing the password
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    })
+                })
+            }
+        });
+})
 
 module.exports = router;
