4917fd25cef897bf9165018578f0853d67c0187a
Shiristy60 <shiristykotnala1.1@gmail.com>
2021-01-17 19:21:44 +0530

    Redux-Tools-Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Route exact path = "/" component = {Landing}/>
-        <div className="container">
-          <Route exact path ="/register" component = {Register} />
-          <Route exact path ="/login" component = { Login } />
+    <Provider store = { store }>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path = "/" component = {Landing}/>
+          <div className="container">
+            <Route exact path ="/register" component = {Register} />
+            <Route exact path ="/login" component = { Login } />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router>
-    
+      </Router>
+    </Provider>
   );
 }
 
